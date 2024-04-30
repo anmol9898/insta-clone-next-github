8c15879f729178157390db81db38b3b8e81ea26e
Anmol Arora <anmolairy98@gmail.com>
2024-04-30 14:39:22 +0530

    Added Modal UI using react-modal

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,15 @@ import logoLong from "./../../public/instagram-long.webp";
 import logoShort from "./../../public/instgram-short.webp";
 import Link from "next/link";
 import { signIn, useSession, signOut } from "next-auth/react";
+import Modal from "react-modal";
+import { useState } from "react";
+import { HiCamera } from "react-icons/hi";
+import { IoCloseOutline } from "react-icons/io5";
 
 const Header = () => {
   const { data: session } = useSession();
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="p-3 shadow-sm border-b sticky top-0 bg-white z-30">
       <div className="flex justify-between items-center max-w-6xl mx-auto">
@@ -52,6 +58,12 @@ const Header = () => {
                 width={60}
                 height={60}
               />
+              <button
+                onClick={() => setIsOpen(true)}
+                className="bg-sky-500 text-white py-3 px-4 rounded-lg shadow-md text-md font-semibold hover:scale-105 transition-all"
+              >
+                Create Post
+              </button>
             </div>
           ) : (
             <Link
@@ -64,6 +76,35 @@ const Header = () => {
           )}
         </div>
       </div>
+
+      {/* Modal */}
+      {isOpen && (
+        <Modal
+          isOpen={isOpen}
+          className="max-w-lg w-[90%] p-6 absolute top-56 left-[50%] translate-x-[-50%] bg-white border-2 rounded-md shadow-md"
+          onRequestClose={() => setIsOpen(false)}
+          ariaHideApp={false}
+        >
+          <div className="absolute right-2 top-2">
+            <IoCloseOutline
+              className="cursor-pointer text-xl text-gray-700 hover:text-red-500 transform hover:scale(125)"
+              onClick={() => setIsOpen(false)}
+            />
+          </div>
+          <div className="flex flex-col justify-center items-center gap-4">
+            <HiCamera className="text-5xl text-gray-400 opacity-90 cursor-pointer" />
+            <input
+              type="text"
+              placeholder="Please enter your caption"
+              maxLength="150"
+              className="w-full text-center active:outline-none focus:outline-none pl-2"
+            />
+            <button disabled className="bg-red-600 w-full p-2 rounded-md text-white text-lg shadow-md hover:brightness-105 disabled:bg-gray-200 disabled:cursor-not-allowed disabled:brightness-100">
+              Upload Post
+            </button>
+          </div>
+        </Modal>
+      )}
     </div>
   );
 };
