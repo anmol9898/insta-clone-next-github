4f6f09c0115e640ca98e8f36e520d662684ae121
Anmol Arora <anmolairy98@gmail.com>
2024-05-06 17:03:56 +0530

    fixed google import

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/Google";
+import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
   // Configure one or more authentication providers
